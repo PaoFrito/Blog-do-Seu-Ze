f20a9624674c182ef0e99f5890d24c6b00099589
Pao <algumacoisaqns@gmail.com>
2021-12-21 14:02:42 -0300

    Slugify & adm "new categories" page
    add lib slugify to the project and created adm page to create new categories

--- controller/categoriesController.js
+++ controller/categoriesController.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const slugify = require('slugify');
+const CategoryModel = require('../model/CategoryModel');
+
+router.get("/adm/categories", (req, res)=>{
+    res.render("../views/adm/categories/index")
+});
 
 router.get("/adm/categories/new", (req, res)=>{
     res.render("../views/adm/categories/new");
+});
+
+router.post("/adm/categories/save", (req, res)=>{
+    var name = req.body.name;
+
+    if(name != undefined){
+        CategoryModel.create({
+            name: name,
+            slug: slugify(name)
+        }).then(()=>{
+            res.redirect()
+        })
+    }
+
+    res.redirect("/adm/categories/new");
 });
 
 module.exports = router;
