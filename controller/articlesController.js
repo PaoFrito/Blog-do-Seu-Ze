6c3639224f427ea406b68d976b14c2c85f76c507
Henrique <henriqueamaralhp@gmail.com>
2021-12-22 16:45:57 -0300

    adm/article route and article => Create

--- controller/articlesController.js
+++ controller/articlesController.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const ArticleModel = require('../model/ArticleModel');
+const slugify = require('slugify');
 
-router.get("/articles", (req, res)=>{
+router.get("/adm/articles", (req, res)=>{
     res.send("articles");
 });
+
+router.get("/adm/articles/new", (req, res)=>{
+    res.render("adm/articles/new");
+});
+
+router.post("/adm/articles/create", (req, res)=>{
+    var title = req.body.title;
+    var content = req.body.content;
+
+    if(title != undefined){
+        ArticleModel.create({
+            title: title,
+            slug: slugify(title),
+            content: content
+        });
+    }
+    res.redirect("/adm/articles");
+});
+
+
 
 module.exports = router;
