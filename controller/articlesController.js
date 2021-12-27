088a7b13cc0e26e438ed6a8c37ba2935566b8cfa
Henrique <henriqueamaralhp@gmail.com>
2021-12-27 16:38:41 -0300

    article => Create with it's categoty

--- controller/articlesController.js
+++ controller/articlesController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const ArticleModel = require('../model/ArticleModel');
+const CategoryModel = require('../model/CategoryModel');
 const slugify = require('slugify');
 
 router.get("/adm/articles", (req, res)=>{
@@ -8,18 +9,22 @@ router.get("/adm/articles", (req, res)=>{
 });
 
 router.get("/adm/articles/new", (req, res)=>{
-    res.render("adm/articles/new");
+    CategoryModel.findAll().then(category =>{
+        res.render("adm/articles/new", {category: category});
+    });
 });
 
 router.post("/adm/articles/create", (req, res)=>{
     var title = req.body.title;
     var content = req.body.content;
+    var categoryId = req.body.categoryId;
 
     if(title != undefined){
         ArticleModel.create({
             title: title,
             slug: slugify(title),
-            content: content
+            content: content,
+            categoryId: categoryId
         });
     }
     res.redirect("/adm/articles");
